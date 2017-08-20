7e7b891f629c52c5a0b2f60c2532ee48e0991762
OprisHoria <opris92@gmail.com>
2017-08-20 18:45:28 +0300

    Implement hello world output by pressing button

--- app/src/main/java/opris/horia/calculator/MainActivity.java
+++ app/src/main/java/opris/horia/calculator/MainActivity.java
@@ -1,7 +1,9 @@
 package opris.horia.calculator;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        EditText e = (EditText) findViewById(R.id.outputTxt);
+
+        e.setText("Hello world");
     }
 }
